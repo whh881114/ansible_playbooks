0b215d548575f0d7cfba39e2c68c95a951bf1f0a
whh881114@gmail.com <54007@beyOnd>
2022-02-02 09:33:24 +0800

    将k8s相关文件全部移出

--- dockerfiles/common-centos7/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-registry_url="harbor.freedom.org"
-registry_project="freedom"
-image="$registry_url"/"$registry_project"/common-centos7:latest
-
-docker build -t $image .
-docker push $image
--- dockerfiles/jenkins-centos7/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-registry_url="harbor.freedom.org"
-registry_project="freedom"
-image="$registry_url"/"$registry_project"/jenkins-centos7:2.319.1-1.1
-
-docker build -t $image .
-docker push $image
